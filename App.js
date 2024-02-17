57da0b299c67833e4b63d433cf345f9351770c5f
Chuck Joshua Mbanaso <chuksjoshuaa@gmail.com>
2024-02-17 10:58:31 +0100

    fix minor changes

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 // React and React Native imports
-import React, { useState } from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import React, { useState } from "react";
+import { Platform, StatusBar, StyleSheet, View } from "react-native";
 
 // Expo imports
-import { AppLoading } from 'expo';
-import { Asset } from 'expo-asset';
-import * as Font from 'expo-font';
-import { Ionicons } from '@expo/vector-icons';
+import { AppLoading } from "expo";
+import { Asset } from "expo-asset";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
 
 // Navigation imports
-import AppNavigator from './navigation/AppNavigator';
+import AppNavigator from "./navigation/AppNavigator";
 
 // App component
 const App = (props) => {
@@ -26,23 +26,23 @@ const App = (props) => {
   } else {
     return (
       <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+        {Platform.OS === "ios" && <StatusBar barStyle="default" />}
         <AppNavigator />
       </View>
     );
   }
-}
+};
 
 // Resource loading
 const loadResourcesAsync = async () => {
   await Promise.all([
     Asset.loadAsync([
-      require('./assets/images/robot-dev.png'),
-      require('./assets/images/robot-prod.png'),
+      require("./assets/images/robot-dev.png"),
+      require("./assets/images/robot-prod.png"),
     ]),
     Font.loadAsync({
       ...Ionicons.font,
-      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+      "space-mono": require("./assets/fonts/SpaceMono-Regular.ttf"),
     }),
   ]);
 };
@@ -61,9 +61,8 @@ const handleFinishLoading = (setLoadingComplete) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
 });
 
-
-export default App
+export default App;
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
-module.exports = function(api) {
+var path = require("path");
+
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            resolver: path.resolve(__dirname, "resolver/react-native"),
+          },
+        },
+      ],
+
+      "react-native-reanimated/plugin",
+    ],
   };
 };
