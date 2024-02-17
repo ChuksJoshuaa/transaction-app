4d08b8cfe9a4dbcbc81305ab16696aac36461819
Chuck Joshua Mbanaso <chuksjoshuaa@gmail.com>
2024-02-17 14:27:52 +0100

    Cleanup transfer screen ui

--- screens/TransactionScreen.js
+++ screens/TransactionScreen.js
@@ -1,9 +1,15 @@
-
-import { Body, Button, Header, Icon, Left, Right, Title } from 'native-base';
-import React from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
-import { Block, Card, Label, Text } from '../components';
-import * as theme from '../constants/theme';
+import { Body, Button, Header, Icon, Left, Right, Title } from "native-base";
+import React from "react";
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { Block, Card, Label, Text } from "../components";
+import * as theme from "../constants/theme";
+import { TransactionData } from "../utils";
+import moment from "moment";
 
 const TransactionScreen = ({ navigation }) => {
   return (
@@ -21,9 +27,11 @@ const TransactionScreen = ({ navigation }) => {
       </Header>
       <SafeAreaView style={styles.overview}>
         <ScrollView contentContainerStyle={{ paddingVertical: 25 }}>
-            <Card row middle style={styles.margin}>
+          <Card row middle style={styles.margin}>
             <Block flex={1.2} center middle style={{ marginRight: 0 }}>
-              <Text light height={43} size={26} spacing={-0.45}>Chuck Mbanaso</Text>
+              <Text light height={43} size={26} spacing={-0.45}>
+                Chuck Mbanaso
+              </Text>
               <Text ligth caption center style={{ marginTop: 3 }}>
                 Account Number: 0078675640
               </Text>
@@ -32,10 +40,12 @@ const TransactionScreen = ({ navigation }) => {
               </Text>
             </Block>
           </Card>
-          
+
           <Card row middle style={[styles.margin, { marginTop: 18 }]}>
             <Block flex={1.2} center middle style={{ marginRight: 0 }}>
-              <Text light height={43} size={36} spacing={-0.45}>#1, 800.00</Text>
+              <Text light height={43} size={36} spacing={-0.45}>
+                #1, 800.00
+              </Text>
               <Text ligth caption center style={{ marginTop: 3 }}>
                 AVAILABLE BALANCE
               </Text>
@@ -44,7 +54,9 @@ const TransactionScreen = ({ navigation }) => {
 
           <Card row middle style={[styles.margin, { marginTop: 18 }]}>
             <Block flex={1.2} center middle style={{ marginRight: 0 }}>
-              <Text light height={43} size={36} spacing={-0.45}>#1, 800.00</Text>
+              <Text light height={43} size={36} spacing={-0.45}>
+                #1, 800.00
+              </Text>
               <Text ligth caption center style={{ marginTop: 3 }}>
                 SPENT
               </Text>
@@ -54,14 +66,22 @@ const TransactionScreen = ({ navigation }) => {
           <Block row style={[styles.margin, { marginTop: 18 }]}>
             <Card middle style={{ marginRight: 7 }}>
               {/* <IconImage vehicle /> */}
-              <Text h2 style={{ marginTop: 17 }}>1,428</Text>
-              <Text paragraph color="gray">Vehicles on track</Text>
+              <Text h2 style={{ marginTop: 17 }}>
+                1,428
+              </Text>
+              <Text paragraph color="gray">
+                Vehicles on track
+              </Text>
             </Card>
-            
+
             <Card middle style={{ marginLeft: 7 }}>
               {/* <IconImage distance /> */}
-              <Text h2 style={{ marginTop: 17 }}>158.3</Text>
-              <Text paragraph color="gray">Distance driven</Text>
+              <Text h2 style={{ marginTop: 17 }}>
+                158.3
+              </Text>
+              <Text paragraph color="gray">
+                Distance driven
+              </Text>
             </Card>
           </Block>
 
@@ -72,11 +92,15 @@ const TransactionScreen = ({ navigation }) => {
             <Block row right>
               <Block flex={2} row center right>
                 <Label blue />
-                <Text paragraph color="gray">Today</Text>
+                <Text paragraph color="gray">
+                  Today
+                </Text>
               </Block>
               <Block row center right>
                 <Label purple />
-                <Text paragraph color="gray">Yesterday</Text>
+                <Text paragraph color="gray">
+                  Yesterday
+                </Text>
               </Block>
             </Block>
             <Block>
@@ -84,56 +108,34 @@ const TransactionScreen = ({ navigation }) => {
             </Block>
           </Card>
 
-        <TouchableOpacity>
-          <Card
-            title="TRANSACTION INFORMATION"
-            style={[styles.margin, { marginTop: 18 }]}
-          >
-            <Block style={styles.driver}>
-              <TouchableOpacity activeOpacity={0.8}>
-                <Block row center>
-                  <Block flex={2}>
-                    <Text h4>Chukwudi Joshua</Text>
-                    <Text paragraph color="gray">Wema Bank</Text>
-                  </Block>
-                  <Block>
-                    <Text paragraph right color="black">$6,432</Text>
-                    <Text paragraph right color="gray">Yesterday</Text>
-                  </Block>
-                </Block>
-              </TouchableOpacity>
-            </Block>
-            <Block style={styles.driver}>
-              <TouchableOpacity activeOpacity={0.8}>
-                <Block row center>
-                  <Block flex={2}>
-                    <Text h4>Ebuka John</Text>
-                    <Text paragraph color="gray">UBA bank</Text>
-                  </Block>
-                  <Block>
-                    <Text paragraph right color="black">$6,432</Text>
-                    <Text paragraph right color="gray">02/16/2024</Text>
+          <TouchableOpacity>
+            <Card
+              title="TRANSACTION INFORMATION"
+              style={[styles.margin, { marginTop: 18 }]}
+            >
+              {TransactionData.map((item) => (
+                <Block style={styles.driver} activeOpacity={0.8} key={item.id}>
+                  <Block row center>
+                    <Block flex={2}>
+                      <Text h4>{item.name}</Text>
+                      <Text paragraph color="gray">
+                        {item.bank}
+                      </Text>
+                    </Block>
+                    <Block>
+                      <Text paragraph right color="black">
+                        {item.amount}
+                      </Text>
+                      <Text paragraph right color="gray">
+                        {moment(item.timestamp).format("MMM DD")} -{" "}
+                        {moment(item.timestamp).format("LTS")}
+                      </Text>
+                    </Block>
                   </Block>
                 </Block>
-              </TouchableOpacity>
-            </Block>
-            <Block style={styles.driver}>
-              <TouchableOpacity activeOpacity={0.8}>
-                <Block row center>
-                  <Block flex={2}>
-                    <Text h4>Jane Ufoma</Text>
-                    <Text paragraph color="gray">Sterling bank</Text>
-                  </Block>
-                  <Block>
-                    <Text paragraph right color="black">$6,432</Text>
-                    <Text paragraph right color="gray">02/17/2024</Text>
-                  </Block>
-                </Block>
-              </TouchableOpacity>
-            </Block>
-          </Card>
+              ))}
+            </Card>
           </TouchableOpacity>
-
 
           <Card
             title="TRIPS BY TYPE"
@@ -144,17 +146,25 @@ const TransactionScreen = ({ navigation }) => {
             </Block>
             <Block row space="between" style={{ marginTop: 25 }}>
               <Block>
-                <Text h2 light>1,744</Text>
+                <Text h2 light>
+                  1,744
+                </Text>
                 <Block row center>
                   <Label blue />
-                  <Text paragraph color="gray">Confort</Text>
+                  <Text paragraph color="gray">
+                    Confort
+                  </Text>
                 </Block>
               </Block>
               <Block>
-                <Text h2 light>2,312</Text>
+                <Text h2 light>
+                  2,312
+                </Text>
                 <Block row center>
                   <Label purple />
-                  <Text paragraph color="gray">Premium</Text>
+                  <Text paragraph color="gray">
+                    Premium
+                  </Text>
                 </Block>
               </Block>
             </Block>
@@ -165,11 +175,10 @@ const TransactionScreen = ({ navigation }) => {
   );
 };
 
-
 const styles = StyleSheet.create({
   overview: {
     flex: 1,
-    flexDirection: 'column',
+    flexDirection: "column",
     backgroundColor: theme.colors.white,
   },
   margin: {
@@ -185,8 +194,8 @@ const styles = StyleSheet.create({
   },
   headerText: {
     flex: 4,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
